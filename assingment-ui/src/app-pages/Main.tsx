47312a5160ace2be24297bdef514bc71badaec9a
shakya <shakya980528@gmail.com>
2022-10-01 13:47:36 -0700

    Completing the challenge - Challenge 2

--- assingment-ui/src/app-pages/Main.tsx
+++ assingment-ui/src/app-pages/Main.tsx
@@ -22,7 +22,7 @@ export default function Main() {
           borderColor: "divider",
         }}
       >
-        <Typography sx={{textAlign: "CenterFocusStrong",}} variant="h3" gutterBottom>
+        <Typography variant="h3" gutterBottom>
           Back-End Coding Challenges
         </Typography>
       </Box>
@@ -35,7 +35,24 @@ export default function Main() {
           borderColor: "divider",
         }}
       >
+        <Typography variant="h4" gutterBottom>
+          Challenge 01
+        </Typography>
         <ChallengeOneComponent />
+      </Box>
+      <Box
+        sx={{
+          px: "20px",
+          pb: 4,
+          pt: 2.5,
+          borderBottom: 2,
+          borderColor: "divider",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Challenge 02
+        </Typography>
+        <ChallengeTwoComponent />
       </Box>
     </>
   );
--- assingment-ui/src/challenges/ChallengeTwoComponent.tsx
+++ assingment-ui/src/challenges/ChallengeTwoComponent.tsx
@@ -1,24 +1,52 @@
-import * as React from 'react';
+import { useRef, useState } from "react";
+import { Box, Grid, Typography, Button } from "@mui/material";
 
-export interface IChallengeTwoComponentProps {
-}
+export default function ChallengeTwoComponent() {
+  var array = Array();
+  const [displayArray, setDisplayArray] = useState<any[]>();
+  const [occuranceArray, setOccuranceArray] = useState<any[]>();
+  const inputRef = useRef<any>(null);
 
-export interface IChallengeTwoComponentState {
-}
-
-export default class ChallengeTwoComponent extends React.Component<IChallengeTwoComponentProps, IChallengeTwoComponentState> {
-  constructor(props: IChallengeTwoComponentProps) {
-    super(props);
-
-    this.state = {
-    }
+  function handleClick() {
+    inputRef.current !== null && array.push(parseInt(inputRef.current.value));
+    inputRef.current.value = "";
   }
 
-  public render() {
-    return (
-      <div>
-        
-      </div>
-    );
+  function calculateOccurance() {
+    setDisplayArray(array);
+    const filtered = array.filter((v, i) => array.indexOf(v) !== i);
+    const unique = new Set(filtered);
+    const occuranceSet = Array.from(unique);
+    setOccuranceArray(occuranceSet);
+    array = [];
   }
+
+  return (
+    <Box component={"div"}>
+      <Grid item xs={12} sm={12}>
+        <input type="text" ref={inputRef} />
+        <Button
+          variant="contained"
+          id="btn1"
+          value="Add"
+          onClick={handleClick}
+          sx={{ height: "30px", ml: 3 }}
+        >
+          Enter the number
+        </Button>
+      </Grid>{" "}
+      <Grid>
+        <Button variant="contained" onClick={calculateOccurance} sx={{ mt: 3 }}>
+          Proceed
+        </Button>
+        <Typography variant="h6" sx={{ mt: 3 }}>
+          {" "}
+          Numbers you have Entered : {displayArray?.join(", ")}
+        </Typography>
+        <Typography variant="h6" sx={{ mt: 3 }}>
+          Numbers you have repeated : {occuranceArray?.join(", ")}
+        </Typography>
+      </Grid>
+    </Box>
+  );
 }
